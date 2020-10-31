d99dd5222bde346e56e8e20f5b0224466468e984
Hikari Hayashi <rev.hikari@gmail.com>
2020-10-31 22:10:11 +0800

    Reduce peer dependencies of eslint config

---
+++ packages/eslint-config/src/index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+
+      parser: require.resolve("@typescript-eslint/parser"),
+      parserOptions: {
+        project: ["./tsconfig.json", "./packages/*/tsconfig.json"],
+      },
+
+      plugins: ["@typescript-eslint", "react-hooks"],
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:react-hooks/recommended",
+        require.resolve("eslint-config-prettier"),
+        require.resolve("eslint-config-prettier/@typescript-eslint"),
+      ],
+
+      rules: {
+        eqeqeq: "error",
+      },
+    },
+
+    {
+      files: ["*.js"],
+
+      parser: require.resolve("@babel/eslint-parser"),
+      parserOptions: {
+        sourceType: "unambiguous",
+      },
+    },
+  ],
+};
