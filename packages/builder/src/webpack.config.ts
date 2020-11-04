592a00eb86088a65d9dc74211cc2df860555dc1b
Hikari Hayashi <rev.hikari@gmail.com>
2020-11-04 22:17:44 +0800

    Define env variables

--- packages/builder/src/webpack.config.ts
+++ packages/builder/src/webpack.config.ts
@@ -3,6 +3,11 @@ import { createConfig } from "@buncho/webpack-config";
 const config = createConfig({
   host: process.env.HOST,
   port: Number(process.env.PORT),
+  env: Object.fromEntries(
+    Object.entries(process.env).filter(([key]) => {
+      return key.startsWith("APP_");
+    }),
+  ),
 });
 
 export default config;
