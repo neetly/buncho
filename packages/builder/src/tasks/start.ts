99de0c9f7b49e88959088bc2d698d7002b0ea123
Hikari Hayashi <rev.hikari@gmail.com>
2020-11-04 18:43:17 +0800

    Fix builder script

--- packages/builder/src/tasks/start.ts
+++ packages/builder/src/tasks/start.ts
@@ -3,7 +3,7 @@ import { execute } from "../utils/execute";
 const start = async (): Promise<void> => {
   await execute({
     path: require.resolve("webpack-cli/bin/cli.js"),
-    args: ["--config", require.resolve("../webpack.config"), "serve"],
+    args: ["serve", "--config", require.resolve("../webpack.config")],
     env: {
       NODE_ENV: "development",
     },
