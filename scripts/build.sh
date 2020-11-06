ff56211b3c620d9aee37e98b7308039fc049dd97
Hikari Hayashi <rev.hikari@gmail.com>
2020-11-07 00:04:33 +0800

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,6 @@ ROOT="./packages/${1:?}"
 
 yarn babel \
   --extensions .ts,.tsx \
-  --ignore "$ROOT/src/**/*.d.ts" \
   --copy-files \
   "$ROOT/src" --out-dir "$ROOT/lib"
 yarn tsc --build "$ROOT"
