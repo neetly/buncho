9171015bb5090e3ffcac09a922c49c824e929742
Hikari Hayashi <rev.hikari@gmail.com>
2020-11-06 23:52:15 +0800

    Use dotenv for storybook

--- packages/builder/src/config/storybook/main.ts
+++ packages/builder/src/config/storybook/main.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import type { Configuration } from "webpack";
 
+import "@buncho/dotenv";
 import { createStorybookConfig } from "@buncho/webpack-config";
 
 export const stories = [path.resolve("./src/**/*.stories.{ts,tsx}")];
