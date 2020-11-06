c4781bd7854495e62900d63e9861db41857db3ea
Hikari Hayashi <rev.hikari@gmail.com>
2020-11-06 18:00:18 +0800

    Update storybook config

--- packages/builder/src/storybook/webpack.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Configuration } from "webpack";
-
-import { createStorybookConfig } from "@buncho/webpack-config";
-
-export default ({ config }: { config: Configuration }): Configuration => {
-  return createStorybookConfig(config);
-};
