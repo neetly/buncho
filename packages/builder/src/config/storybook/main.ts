fb733d0675ec3ac7e9a18c9b523eabce7c391ede
Hikari Hayashi <rev.hikari@gmail.com>
2020-11-08 00:12:20 +0800

    Remove unused dotenv

--- packages/builder/src/config/storybook/main.ts
+++ packages/builder/src/config/storybook/main.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import type { Configuration } from "webpack";
 
-import "@buncho/dotenv";
 import { createStorybookConfig } from "@buncho/webpack-config";
 
 export const stories = [path.resolve("./src/**/*.stories.{ts,tsx}")];
