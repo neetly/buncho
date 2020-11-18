1874f9cb25457f0ccfa49782e97fcde89ccccd00
Hikari Hayashi <rev.hikari@gmail.com>
2020-11-18 16:54:44 +0800

    Add default host and port

--- packages/core/builder/src/config/webpack.config.ts
+++ packages/core/builder/src/config/webpack.config.ts
@@ -2,8 +2,8 @@ import "@buncho/dotenv";
 import { createConfig } from "@buncho/webpack-config";
 
 const config = createConfig({
-  host: process.env.HOST,
-  port: Number(process.env.PORT),
+  host: process.env.HOST || "localhost",
+  port: Number(process.env.PORT || 3000),
   env: Object.fromEntries(
     Object.entries(process.env).filter(([key]) => {
       return key.startsWith("APP_");
