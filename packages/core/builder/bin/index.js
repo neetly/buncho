87bcece859cbc59d2ccda144a2cdcbc35daaf8e7
Hikari Hayashi <rev.hikari@gmail.com>
2021-01-04 05:09:01 +0800

    Revert "Clean up"
    This reverts commit 8c4889aed5c5e6899042abcae7ea438752cc9394.

--- packages/core/builder/bin/index.js
+++ packages/core/builder/bin/index.js
@@ -1,4 +1,16 @@
-module.exports = require("../lib");
+let useRegister = false;
+
+try {
+  require.resolve("../src/index.ts");
+  useRegister = true;
+} catch {} // eslint-disable-line no-empty
+
+if (useRegister) {
+  require("@buncho/register");
+  module.exports = require("../src");
+} else {
+  module.exports = require("../lib");
+}
 
 process.on("unhandledRejection", (error) => {
   if (error) console.error(error);
