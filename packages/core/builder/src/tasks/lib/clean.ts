2fb5e59ee08fbb7c2b5d5573f296c301b3c4c21a
Hikari Hayashi <rev.hikari@gmail.com>
2021-03-29 20:58:51 +0800

    Improve clean tasks

--- packages/core/builder/src/tasks/lib/clean.ts
+++ packages/core/builder/src/tasks/lib/clean.ts
@@ -6,11 +6,11 @@ import { execute } from "../../utils/execute";
 import { getPackageBin } from "../../utils/getPackageBin";
 
 const clean: Task = async () => {
-  await fs.rmdir(path.resolve("./lib"), { recursive: true });
   await execute({
     path: getPackageBin("typescript", "tsc"),
     args: ["--build", "--clean", path.resolve("./tsconfig.json")],
   });
+  await fs.rmdir(path.resolve("./lib"), { recursive: true });
 };
 
 export { clean };
--- packages/core/builder/src/tasks/storybook/clean.ts
+++ packages/core/builder/src/tasks/storybook/clean.ts
@@ -2,8 +2,15 @@ import { promises as fs } from "fs";
 import path from "path";
 
 import type { Task } from "../../types/Task";
+import { execute } from "../../utils/execute";
+import { getPackageBin } from "../../utils/getPackageBin";
 
 const clean: Task = async () => {
+  await execute({
+    path: getPackageBin("typescript", "tsc"),
+    args: ["--build", "--clean", path.resolve("./tsconfig.json")],
+  });
+  await fs.rmdir(path.resolve("./lib"), { recursive: true });
   await fs.rmdir(path.resolve("./storybook-static"), { recursive: true });
 };
 
