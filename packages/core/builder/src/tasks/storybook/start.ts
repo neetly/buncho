488b17025e283ff2da5514f9dda907fc7ae3720b
Hikari Hayashi <rev.hikari@gmail.com>
2021-03-29 21:00:56 +0800

    Allow customize storybook host and port by env

--- packages/core/builder/src/tasks/storybook/start.ts
+++ packages/core/builder/src/tasks/storybook/start.ts
@@ -6,7 +6,11 @@ import { getPackageBin } from "../../utils/getPackageBin";
 const start: Task = async () => {
   await execute({
     path: getPackageBin("@storybook/react", "start-storybook"),
-    args: ["--config-dir", paths.storybookConfigDir],
+    args: [
+      ["--config-dir", paths.storybookConfigDir],
+      ["--host", process.env.STORYBOOK_HOST || "localhost"],
+      ["--port", process.env.STORYBOOK_PORT || "9000"],
+    ].flat(),
   });
 };
 
