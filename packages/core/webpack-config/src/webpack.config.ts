d5b5242d333da46cae9a93cfc1e5fe1a9fedcd00
Hikari Hayashi <rev.hikari@gmail.com>
2021-04-21 00:25:01 +0800

    Update webpack.config.ts

--- packages/core/webpack-config/src/webpack.config.ts
+++ packages/core/webpack-config/src/webpack.config.ts
@@ -49,7 +49,6 @@ const config: Configuration = {
   resolve: {
     extensions: EXTENSIONS,
     alias: utils.getWebpackAlias(),
-    // @ts-expect-error Webpack
     plugins: hasTsconfigPaths ? [new TsconfigPathsPlugin()] : [],
   },
 
