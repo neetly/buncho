2b9e6a634a08717cbe8e095862338c10ef5381c0
Hikari Hayashi <rev.hikari@gmail.com>
2021-10-22 07:45:48 +0800

    Remove router package

--- packages/core/router/src/ActionContext.ts
+++
@@ -1,6 +0,0 @@
-import type { Action } from "history";
-import { createContext } from "react";
-
-const ActionContext = createContext<Action | null>(null);
-
-export { ActionContext };
--- packages/core/router/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { BrowserRouter } from "./BrowserRouter";
-export { HashRouter } from "./HashRouter";
-export { useAction } from "./useAction";
-export { useScrollRestoration } from "./useScrollRestoration";
-export * from "react-router-dom";
