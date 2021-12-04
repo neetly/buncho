f8df5976a830a438f293297bf28e391f39caa62f
Hikari Hayashi <rev.hikari@gmail.com>
2021-12-04 10:17:22 +0800

    Support csf3

--- packages/core/builder/src/config/storybook/main.ts
+++ packages/core/builder/src/config/storybook/main.ts
@@ -4,7 +4,7 @@ import { createStorybookConfig } from "@buncho/webpack-config";
 import path from "path";
 import type { Configuration } from "webpack";
 
-export const stories = [path.resolve("./src/**/*.stories.{ts,tsx,mts,cts}")];
+export const stories = [path.resolve("./src")];
 
 export const core = { builder: "webpack5" };
 export const addons = ["@storybook/addon-essentials"];
