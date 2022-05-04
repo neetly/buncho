8643419333ad0907657d75b3477d3999c6df8ed9
Hikari Hayashi <rev.hikari@gmail.com>
2022-05-04 15:52:18 +0800

    Support .storybook/preview-{head,body}.html

--- packages/builder/src/config/storybook/main.ts
+++ packages/builder/src/config/storybook/main.ts
@@ -17,6 +17,28 @@ export const features = {
   storyStoreV7: process.env.STORYBOOK_STORE_V7 === "true",
 };
 
+const appConfigDir = process.env.APP_STORYBOOK_CONFIG_DIR;
+
+export const previewHead = (head: string) => {
+  if (appConfigDir) {
+    const previewHeadFile = path.join(appConfigDir, "preview-head.html");
+    if (fs.existsSync(previewHeadFile)) {
+      return head + fs.readFileSync(previewHeadFile).toString();
+    }
+  }
+  return head;
+};
+
+export const previewBody = (body: string) => {
+  if (appConfigDir) {
+    const previewBodyFile = path.join(appConfigDir, "preview-body.html");
+    if (fs.existsSync(previewBodyFile)) {
+      return body + fs.readFileSync(previewBodyFile).toString();
+    }
+  }
+  return body;
+};
+
 export const webpackFinal = (defaultConfig: Configuration) => {
   return createStorybookConfig(defaultConfig, {
     env: Object.fromEntries(
