5710f4f57550c87879f0f207af48ed586a6cdf1c
Hikari Hayashi <rev.hikari@gmail.com>
2022-10-15 18:34:26 +0800

    Add webpack env package

---
+++ packages/webpack-env/src/index.d.ts
@@ -0,0 +1,32 @@
+/// <reference lib="DOM" />
+/// <reference lib="DOM.Iterable" />
+
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare interface ImportMeta {
+  url: string;
+}
+
+// eslint-disable-next-line no-var
+declare var process: NodeJS.Process;
+
+declare namespace NodeJS {
+  interface Process {
+    env: ProcessEnv;
+  }
+
+  interface ProcessEnv {
+    NODE_ENV: "production" | "development" | "test";
+  }
+}
+
+declare module "*.css" {
+  const exports: Record<string, string>;
+  export default exports;
+}
+
+declare module "*.scss" {
+  const exports: Record<string, string>;
+  export default exports;
+}
--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ yarn run --top-level tsc --build .
 yarn run --top-level babel \
   --root-mode upward \
   --extensions .js,.mjs,.cjs,.ts,.tsx,.mts,.cts \
+  --ignore "**/*.d.ts","**/*.d.mts","**/*.d.cts" \
   --source-maps true \
   --copy-files \
   ./src --out-dir ./lib
